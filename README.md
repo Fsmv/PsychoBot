02add005de4a64d42acfa50d9383d522a6565924
Andrew Kallmeyer <fsmv@sapium.net>
2015-06-29 04:32:01 +0000

    Switched to json for configuration

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Dependencies
 ============
+
  `sudo apt-get install cmake check lubmicrohttpd-dev lua5.2-dev lubcurlpp-dev lubcurl4-openssl-dev`
+ 
+Configuration
+=============
+
+Create config.json in the same directory as the executable and add the following fields:
+
+```
+{
+    "token"       : "YOUR TOKEN",
+    "api_url"     : "https://api.telegram.org/bot",
+    "webhook_url" : "YOUR SERVER URL"
+}
+```
--- src/main.cpp
+++ src/main.cpp
@@ -25,11 +25,37 @@ extern "C" {
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
+#include "json.hpp"
+using json = nlohmann::json;
+
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <iterator>
 
 #include "webhooks.h"
-#include "conf.h"
+
+static const std::string CONFIG_FILE = "config.json";
+static json config;
+
+bool loadConfig() {
+    std::ifstream f(CONFIG_FILE);
+    if(!f.good()) {
+        return false;
+    }
+    
+    std::istream_iterator<char> it(f), eof;
+    std::string config_json(it, eof);
+    config = json::parse(config_json);
+    
+    if (config.find("token") == config.end() ||
+        config.find("api_url") == config.end() ||
+        config.find("webhook_url") == config.end()) {
+        return false;        
+    }
+    
+    return true;
+}
 
 void luaHello() {
     lua_State *L = luaL_newstate();
@@ -46,9 +72,9 @@ void telegramHello() {
     try {
         curlpp::Cleanup myCleanup;
         curlpp::Easy myRequest;
-        myRequest.setOpt<Url>(TG_URL + TG_TOKEN + "/setWebhook");
+        myRequest.setOpt<Url>(config["api_url"].get<std::string>() + config["token"].get<std::string>() + "/setWebhook");
         curlpp::Forms formParts;
-        formParts.push_back(new curlpp::FormParts::Content("url", TG_WEBHOOK_URL));
+        formParts.push_back(new curlpp::FormParts::Content("url", config["webhook_url"].get<std::string>()));
         myRequest.setOpt<HttpPost>(formParts);
         myRequest.perform();
     }
@@ -63,6 +89,11 @@ void telegramHello() {
 }
 
 int main(int argc, char **argv) {
+    if(!loadConfig()) {
+        std::cerr << "Could not load config" << std::endl;
+        return 1;
+    }
+    
     //luaHello();
     telegramHello();
     
